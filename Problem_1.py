f98f5e3a59ff77c48cc607e21e42f07bcf5dadba
Satish Mallikarjun Paraddi <satishparaddi2@gmail.com>
2026-09-13 16:16:13 -0400

    Complete Problem_1.py

---
+++ Problem_1.py
@@ -0,0 +1,21 @@
+#Time Complexity : O(n)
+#Space Complexity : O(1)
+#Did this code successfully run on Leetcode :Yes
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        result = [1]*len(nums)
+        prefix = 1
+
+        for i in range(n):
+            result[i] *= prefix
+            prefix *= nums[i]
+        
+        suffix = 1
+        for i in range(n-1,-1,-1):
+            result[i] *= suffix
+            suffix *= nums[i]
+        
+        return result
+            
+            
